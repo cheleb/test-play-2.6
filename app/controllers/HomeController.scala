035e38066429b96b9b3ae622e6429aec05f04994
Olivier NOUGUIER <olivier.nouguier@hawk-platform.io>
2017-05-09 14:56:12 +0200

    Init

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject.Inject
+
+
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+
+
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def index = Action { implicit request =>
+    Ok(views.html.index())
+  }
+}
